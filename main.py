d032de1234ad64b4da79574b76e843aef09cf445
sistemasmarcelocastro <sistemas.marcelo.castro@gmail.com>
2017-09-17 09:57:14 -0300

    curso flask hasta formularios 2

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask 
 from flask import render_template
+from flask import request
 
 import forms
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<nombre>')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/<nombre>', methods = ['GET', 'POST'])
 def index(nombre='SALAME!'):
 	listanombres = ['vos','dios','todos']
-	formulario1 = forms.Formulario1()
+	formulario1 = forms.Formulario1(request.form)
+
+	if request.method == 'POST':
+		print formulario1.usuario.data
+		print formulario1.correo.data
+		print formulario1.comentario.data
+		
 	return render_template('index.html', nombre=nombre, milista=listanombres, formulario1=formulario1)
 
 
